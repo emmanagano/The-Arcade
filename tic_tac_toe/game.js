3cb2afab543055add681d20461ac890abd1a17c9
Emma Ruth Nagaño <emmaruthnagano@Emmas-MacBook-Pro.local>
2021-12-15 01:51:55 -0500

    revised js code

--- tic_tac_toe/game.js
+++ tic_tac_toe/game.js
@@ -5,7 +5,6 @@ const resetButton = document.querySelector("#restart-button");
 const startButton = document.querySelector("#start-button");
 const grid = document.querySelector(".grid-container");
 let gridCells = Array.from(document.querySelectorAll(".cell"));
-
 let board = ["","","","","","","","",""];
 
 
@@ -18,6 +17,7 @@ let board = ["","","","","","","","",""];
 
 */
 
+let cellIndex;
 let inputArray = [];
 let arrayX = [];
 let arrayO = [];
@@ -33,56 +33,50 @@ const winningCombo = [
     [2,4,6]
 ];
 
-let gameActive = true;
+startButton.addEventListener("click", main());
 
-startButton.addEventListener("click", function main(){
-    gridCells.forEach( (cell) => {
+function main(){
+    gridCells.forEach(cell => {
         let cellIndex = gridCells.indexOf(cell);
         cell.addEventListener("click", function startGame(){
+            inputArray.push(cell);
             if(inputArray.length % 2 === 1){
                 cell.textContent = "X";
                 turnSwitch.textContent = "O";
-                inputArray.push(cellIndex);
+                arrayX.push(cellIndex);
                 cell.removeEventListener("click",startGame);
-            } else if(inputArray.length % 2 === 0){
+                checkWinner();
+            }else if(inputArray.length % 2 === 0){
                 cell.textContent = "O";
                 turnSwitch.textContent = "X";
-                inputArray.push(cellIndex);
+                arrayO.push(cellIndex);
                 cell.removeEventListener("click",startGame);
+                checkWinner();
             };
-            
-           if(cell.textContent === "X"){
-               arrayX.push(cellIndex);
-               if(arrayX.length === 3){
-                   compareWinner();
-               };
-           } else if(cell.textContent === "O"){
-               arrayO.push(cellIndex);
-               if(arrayO.length === 3){
-                    compareWinner();
-               };
-           };
-           if(inputArray.length === 9){
-                return playerTurn.textContent = "Tie";
-           };
+            if(inputArray.length === 9){
+                playerTurn.textContent = "Draw";
+            };
         });
     });
-});
+};
 
-function compareWinner(){
-    for(let i = 0; i < winningCombo.length; i ++){
-        let winningOuter = winningCombo[i];
-        if(String(winningOuter) === String(arrayX)){
-            playerTurn.textContent = "Player X won!";
-            gameOver();
-        } else if(String(winningOuter) === String(arrayO)){
-            playerTurn.textContent = "Player O won!";
-            gameOver();
+function checkWinner(){ 
+    if(arrayX.length === 3 || arrayO.length === 3){
+        for(let i = 0; i < winningCombo.length; i++){
+            let winningIndex = winningCombo[i];
+            if(String(arrayX) === String(winningIndex)){
+                playerTurn.textContent = "Player X Won!";
+            }else if(String(arrayO) === String(winningIndex)){
+                playerTurn.textContent = "Player O Won!";
+            };
         };
     };
 };
 
-function gameOver(){
-    return gameActive = false;
-};
+
+resetButton.addEventListener("click", function(){
+    window.location.reload();
+});
+
+
 
