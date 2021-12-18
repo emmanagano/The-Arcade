79906d5f3e5e17ef48918e193e2b61603d5469e9
Emma Ruth Nagaño <emmaruthnagano@Emmas-MacBook-Pro.local>
2021-12-17 23:07:01 -0500

    before I make major changes

--- tic_tac_toe/game.js
+++ tic_tac_toe/game.js
@@ -2,25 +2,14 @@
 let playerTurn = document.querySelector("#switch-text");
 let turnSwitch = document.querySelector("#player-turn");
 const resetButton = document.querySelector("#restart-button");
-//const startButton = document.querySelector("#start-button");
-//const grid = document.querySelector(".grid-container");
 let gridCells = Array.from(document.querySelectorAll(".cell"));
+let goButton = document.querySelector("#go-button");
 
-
-
-/*
-- coordinates visual:
-
-[0] [1] [2]
-[3] [4] [5]
-[6] [7] [8]
-
-*/
 
 let inputArray = [];
 let arrayX = [];
 let arrayO = [];
-
+let thereIsWinner = false;
 const winningCombo = [
     [0,1,2],
     [3,4,5],
@@ -32,7 +21,25 @@ const winningCombo = [
     [2,4,6],
 ];
 
-let thereIsWinner = false;
+
+
+let userNameX = document.querySelector("#user-x-name");
+let userNameO = document.querySelector("#user-o-name");
+let playerX = document.querySelector("#player-x");
+let playerO = document.querySelector("#player-o");
+
+goButton.addEventListener("click",function getUser(){
+    let userXValue = document.querySelector("#user-x-name").value;
+    let userOValue = document.querySelector("#user-o-name").value;
+    if(userXValue && userOValue){
+        userNameX.classList.add("display-none");
+        userNameO.classList.add("display-none");
+        playerX.classList.add("user-input");
+        playerX.textContent = `${userXValue}`;
+        playerO.classList.add("user-input");
+        playerO.textContent = `${userOValue}`;
+    };
+});
 
 
 gridCells.forEach(cell => {
@@ -44,12 +51,16 @@ gridCells.forEach(cell => {
             turnSwitch.textContent = "O";
             arrayX.push(cellIndex);
             cell.removeEventListener("click",startGame);
-            checkWinner();
+            playerO.classList.add("underline-name");
+            playerX.classList.remove("underline-name");
+            checkWinner(arrayX);
         }else if(inputArray.length % 2 === 0){
             cell.textContent = "O";
             turnSwitch.textContent = "X";
             arrayO.push(cellIndex);
             cell.removeEventListener("click",startGame);
+            playerX.classList.add("underline-name");
+            playerO.classList.remove("underline-name");
             checkWinner();
         };
         if(thereIsWinner === true){
@@ -63,6 +74,7 @@ gridCells.forEach(cell => {
     });
 });
 
+
 /*
 
 enter our names and have them displayed
@@ -71,38 +83,50 @@ take turns placing our marks in empty spaces
 not be able to place our marks in an occupied space
 be told when a move causes a player to win, or to draw
 start the game over without having to reset the browser
+
+bugs: 
+-   It won't announce the winner if there is more than 3 X's or O's
+    in the board
+- 
+
  
 */
 
 
-function checkWinner(){ 
+function checkWinner(playerArray){ 
     arrayX.sort((a,b) => a - b);
     arrayO.sort((a,b) => a - b);
-    if(arrayX.length == 3 || arrayO.length === 3){
+    console.log("arrayX:",arrayX);
+    console.log("arrayO:",arrayO)
+    //if(arrayX.length == 3 || arrayO.length === 3){
         for(let winningArray of winningCombo){
-            if(String(arrayX) === String(winningArray)){
+            //console.log("winningArray:", winningArray);
+            for(let arrayNumber of playerArray){
+                for(let winningNumber of winningArray){
+                    if(playerArray.includes(winningArray)){
+                        console.log("hi");
+                    }
+                }
+            }
+            if(winningArray.includes(arrayX)){
                 playerTurn.textContent = "Player X won!";
                 thereIsWinner = true;
-                gameOver();
-            }else if(String(arrayO) === String(winningArray)){
+                // console.log("stringwinningarray:",String(winningArray));
+                // console.log("stringarrayx:", String(arrayX));
+                // playerX.classList.add("announce-winner");
+                // playerO.classList.remove("underline-name");
+            }else if(String(winningArray) === String(arrayO)){
                 playerTurn.textContent = "Player O won!";
                 thereIsWinner = true;
-                gameOver();
+                // playerO.classList.add("announce-winner");
+                // playerX.classList.remove("underline-name");
             };
+
         };
-    };
+    //};
 };
 
-function gameOver(){
-    if(thereIsWinner === true){
-        for(let cell of gridCells){
-            cell.textContent = "";
-        };
-        arrayX = [];
-        arrayO = [];
-        inputArray = [];
-    };
-};
+
 
 
 //console.log(checkWinner(arrayX));
